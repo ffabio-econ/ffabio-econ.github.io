bdad40e2e53273b703612932d4d5ab1661f9eaf2
Fabio <ffabio.econ@gmail.com>
2023-11-24 11:33:30 +0100

    Refactor toggleLinks event listener to hide/show nextElement

--- assets/js/hidenshow-text.js
+++ assets/js/hidenshow-text.js
@@ -3,11 +3,14 @@ let toggleLinks = document.getElementsByClassName("toggleLink");
 for (let i = 0; i < toggleLinks.length; i++) {
     toggleLinks[i].addEventListener("click", function(e) {
         e.preventDefault();
-        let text = document.getElementsByClassName("toggleText")[i];
-        if (text.style.display === "none") {
-            text.style.display = "block";
+        let nextElement = this.nextElementSibling;
+        while(nextElement && !nextElement.classList.contains("abstracttxt")) {
+            nextElement = nextElement.nextElementSibling;
+        }
+        if (nextElement.style.display === "none") {
+            nextElement.style.display = "block";
         } else {
-            text.style.display = "none";
+            nextElement.style.display = "none";
         }
     });
 }

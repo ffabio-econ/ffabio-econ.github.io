62d38a954679a6bc510f779e6a654c01f5c9979f
Fabio <ffabio.econ@gmail.com>
2023-11-24 11:35:42 +0100

    Fix toggle display issue in hidenshow-text.js

--- assets/js/hidenshow-text.js
+++ assets/js/hidenshow-text.js
@@ -7,7 +7,7 @@ for (let i = 0; i < toggleLinks.length; i++) {
         while(nextElement && !nextElement.classList.contains("abstracttxt")) {
             nextElement = nextElement.nextElementSibling;
         }
-        if (nextElement.style.display === "none") {
+        if (nextElement.style.display === "none" || nextElement.style.display === "") {
             nextElement.style.display = "block";
         } else {
             nextElement.style.display = "none";

9de2446daa1dfc7486cb8baf3af4fadf5efaf453
Fabio <ffabio.econ@gmail.com>
2023-11-24 11:28:49 +0100

    Refactor variable declarations in hidenshow-text.js

--- assets/js/hidenshow-text.js
+++ assets/js/hidenshow-text.js
@@ -1,9 +1,9 @@
-var toggleLinks = document.getElementsByClassName("toggleLink");
+let toggleLinks = document.getElementsByClassName("toggleLink");
 
-for (var i = 0; i < toggleLinks.length; i++) {
+for (let i = 0; i < toggleLinks.length; i++) {
     toggleLinks[i].addEventListener("click", function(e) {
         e.preventDefault();
-        var text = document.getElementsByClassName("toggleText")[i];
+        let text = document.getElementsByClassName("toggleText")[i];
         if (text.style.display === "none") {
             text.style.display = "block";
         } else {
